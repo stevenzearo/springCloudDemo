00fead8b818533d429432f20c132e04fbbd6781e
steve <2226069315@qq.com>
2019-08-05 11:38:48 +0800

    user service registry

---
+++ serviceUser/src/main/java/steve/spring/cloud/service/user/UserService.java
@@ -0,0 +1,11 @@
+package steve.spring.cloud.service.user;
+
+/**
+ * @Author steve
+ * @Date 2019/8/5
+ * @Time 11:15
+ * @Version 1.0
+ */
+public interface UserService {
+    String userTest(String name);
+}
